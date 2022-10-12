db0908af6f96064c888e10b50b33af0864a07008
liyunlongaaa <a2994300398@gmail.com>
2022-10-12 14:18:53 +0800

    u

--- run_on_colab.sh
+++ run_on_colab.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1  main.py \
 --model_type vit_tiny \
---batch_size 32 \
---train_list list/voxceleb1_train_list \
---val_list list/trials.txt \
---train_path /home/yoos/Downloads \
---val_path /home/yoos/Downloads/voxceleb1 \
---musan_path /data/musan \
---saveckp_freq 5 \
+--batch_size 64 \
+--train_list /content/S3RT/list/voxceleb1_train_list \
+--val_list /content/S3RT/list/trials.txt \
+--train_path /content \
+--val_path /content/voxceleb1 \
+--musan_path /content/musan_split \
+--saveckp_freq 10 \
 --imagenet_pretrain False \
 --audioset_pretrain False \
---lr 1e-3 \
---min_lr 1e-5 \
---output_dir ./output2 \
+--lr 1e-4 \
+--min_lr 1e-6 \
+--output_dir /content/drive/Shareddrives \
 --local_crops_number 4 \
---epochs 100 \
+--epochs 500 \
 --warmup_epochs 10 > log2.txt
